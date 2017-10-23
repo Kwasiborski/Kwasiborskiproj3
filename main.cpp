776b50a356df5e4d8a142a091c36d7a023e6d14e
Kwasiborski <s_kwasiborski@wp.pl>
2017-10-23 15:37:04 +0200

    projekty

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <limits>
 
 using namespace std;
 
@@ -11,7 +12,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    double t, x;
+
+
+    double t1, x1;
     string linia;
     typedef pair <double,double> Probka;
     ifstream plik("sygnal.csv");
@@ -19,18 +22,49 @@ int main(int argc, char* argv[])
     while (getline(plik, linia))
     {
         stringstream ss(linia);
-        ss >> t;
+        ss >> t1;
         ss.ignore();
-        ss >> x;
-        dane.push_back(Probka(t, x));
+        ss >> x1;
+        dane.push_back(Probka(t1, x1));
+    }
+    for (int i=0; i< argc;++i){
+
     }
     plik.close();
     for (int i=1; i<dane.size(); i++)
     {
         cout << dane[i].first << "," << dane[i].second << endl;
     }
+    {
     ofstream plik2 ("out.csv", ios::app);
     for (int i=1; i<dane.size(); i++)
         plik2 << dane[i].first << dane[i].second << endl;
     plik.close();
+    }
 }
+
+
+class Probka
+{
+public:
+    double t;
+    double x;
+    Probka();
+    Probka(double t1, double x1);
+    friend (std::ostream& strumien, const Probka& student);
+    };
+    class sygnal
+     {
+public:
+    probka ();
+    probka(std::vector<Probka>);
+    Sygnal ();
+    double p ;
+    dodajProbke(p: const Probka&);
+    void{
+    iloscProbek();
+    int& operator[](int Probka&);
+    }
+    };
+    friend
+
